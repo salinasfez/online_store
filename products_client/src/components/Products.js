1316db8b705fa3894bda0eb6f3f0d31bc9f2144e
Federico Salinas <federicosalinas@federicos-air.attlocal.net>
2020-02-05 20:11:08 -0600

    installed and edited create react app my way

---
+++ products_client/src/components/Products.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+
+class Products extends Component {
+    render(){
+        return(
+            <div>testing</div>
+        )
+    }
+}
+
+export default Products;
