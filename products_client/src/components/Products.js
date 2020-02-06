e2248536cd01a92e3941e313e8917620d677b4a6
Federico Salinas <federicosalinas@federicos-air.attlocal.net>
2020-02-05 20:44:30 -0600

    I was able to render the product via React

--- products_client/src/components/Products.js
+++ products_client/src/components/Products.js
@@ -2,9 +2,46 @@ import React, {Component} from 'react';
 
 
 class Products extends Component {
+    state = {
+        products: []
+    }
+    componentDidMount(){
+        this.getProducts();
+    }
+    getProducts = () => {
+        fetch('http://localhost:3000/products')
+        .then(response => response.json())
+        .then(json => this.setState({products: json}))
+        .catch(error => console.log(error));
+    }
     render(){
         return(
-            <div>testing</div>
+            <div>
+                <h1>Federico Clothing Co.</h1>
+                <div>
+                    <form>
+                        <label htmlFor='name'>Item</label>
+                        <input type='text' id='name'/><br/>
+                        <label htmlFor='price'>Price</label>
+                        <input type='number' id='price' /><br/>
+                        <label htmlFor='description'>Item Description</label>
+                        <input type='text' id='description' /><br/>
+                        <label htmlFor='image'>Product Image</label>
+                        <input type='text' id='image'/><br/>
+                    </form>
+                </div>
+                {this.state.products.map(product => {
+                    return(
+                        <div>
+                            <div>
+                                <h2>{product.name}</h2>
+                                <h4>{product.price}</h4>
+                                <h4>{product.description}</h4>
+                            </div>
+                        </div>    
+                        )
+                })}
+            </div>
         )
     }
 }
