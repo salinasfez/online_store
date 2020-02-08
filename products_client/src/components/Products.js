572056e2076ae6ebaf5991dec3198c20f475850d
Federico Salinas <federicosalinas@federicos-air.attlocal.net>
2020-02-08 10:18:27 -0600

    I am still styling.. I also still need to do the update/edit method

--- products_client/src/components/Products.js
+++ products_client/src/components/Products.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './Products.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Jumbotron } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 
 class Products extends Component {
@@ -12,7 +12,8 @@ class Products extends Component {
             price: '',
             description: '',
             image: ''
-          }
+          },
+        isInEditMode: false
     }
     componentDidMount(){
         this.getProducts();
@@ -63,44 +64,45 @@ class Products extends Component {
             products: [...this.state.products.slice(0, index), ...this.state.products.slice(index + 1)]
           });
         });
-      };
+      }
     render(){
         return(
             <div className='main-container'>
                 <h1 className='header'>Federico Clothing Co.</h1>
-                {/* <Jumbotron>
-                    Hello World
-                </Jumbotron> */}
-                <div className='flex-container'/>
+                <div className='header-container'>
                 <div className='form'>
-                    <h2>List an item</h2>
-                    <form onSubmit={this.handleSubmit}>
-                        <label htmlFor='name'>&nbsp;Item</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        <input type='text' id='name' value={this.state.formInputs.name} onChange={this.handleChange}/>
-                        <label htmlFor='price'>&nbsp;&nbsp;Price</label>&nbsp;&nbsp;
-                        <input type='number' id='price' value={this.state.formInputs.price} onChange={this.handleChange} /><br/><br/>
-                        <label htmlFor='description'>&nbsp;&nbsp;Description</label>&nbsp;&nbsp;
-                        <input type='text' id='description' value={this.state.formInputs.description} onChange={this.handleChange}/>
-                        <label htmlFor='image'>&nbsp;&nbsp;Image</label>&nbsp;&nbsp;
-                        <input type='text' id='image' value={this.state.formInputs.image} onChange={this.handleChange}/><br/><br/>
-                        <Button variant="primary" id='button' type="submit">
-                        Submit
-                        </Button>
-                    </form>
-                    
+                        <h2 id='list-an-item'>List an item</h2><br />
+                        <form onSubmit={this.handleSubmit}>
+                            <label htmlFor='name'>&nbsp;Item</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <input type='text' id='name' value={this.state.formInputs.name} onChange={this.handleChange}/>
+                            <label htmlFor='price'>&nbsp;&nbsp;Price</label>&nbsp;&nbsp;
+                            <input type='number' id='price' value={this.state.formInputs.price} onChange={this.handleChange} /><br/><br/>
+                            <label htmlFor='description'>&nbsp;&nbsp;Description</label>&nbsp;&nbsp;
+                            <input type='text' id='description' value={this.state.formInputs.description} onChange={this.handleChange}/>
+                            <label htmlFor='image'>&nbsp;&nbsp;Image</label>&nbsp;&nbsp;
+                            <input type='text' id='image' value={this.state.formInputs.image} onChange={this.handleChange}/><br/><br/>
+                            <Button variant="primary" id='button' type="submit">
+                            Submit
+                            </Button>
+                        </form>
+                    </div>
+                    <div className='for-him-her'>
+                        
+                        <button>For Him</button>
+                        <button>For Her</button>
+                    </div>
                 </div>
                 <section className='products'>
                     
                     {/* {this.state.formInputs.name} */}
                     {this.state.products.map((product, index) => {
-                        return(
+                        return( 
                             <div className='product-card'>
                                 <div className='product-image'>
                                     <img src={product.image} alt=''/>
                                 </div>
                                 <div className='product-info'>
                                     <h4>{product.name}</h4>
-                                    {/* <h4>{product.description}</h4> */}
                                     <h5>${product.price}</h5>
                                     <h6 onClick={() => this.deleteProduct(product._id, index)}>Remove Item</h6>
                                 </div>
